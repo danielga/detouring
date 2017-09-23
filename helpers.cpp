0e55bac42ca243f62aae4faf5005116ce5d00561
Daniel <danielga@outlook.pt>
2017-09-23 01:23:30 +0100

    Fixed helpers.cpp including classproxy.hpp needlessly.

--- helpers.cpp
+++ helpers.cpp
@@ -33,7 +33,7 @@
 * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *************************************************************************/
 
-#include "classproxy.hpp"
+#include "helpers.hpp"
 
 #if defined _WIN32
 
